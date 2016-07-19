efd5c137247ed0506b2fd8afb093fad22346843a
Robeszpierre <robeszpierre@gmail.com>
2016-07-19 22:41:41 +0200

    routeing finished

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingListAddComponent } from './shopping-list-add.component';
+import { ShoppingListService } from './shopping-list.service';
+import { Ingredient } from '../shared';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-shopping-list',
+  templateUrl: 'shopping-list.component.html',
+  directives: [ShoppingListAddComponent]
+})
+export class ShoppingListComponent implements OnInit {
+    items: Ingredient[];
+  constructor(private sls: ShoppingListService) {}
+
+  ngOnInit() {
+      this.items = this.sls.getItems();
+  }
+
+}
